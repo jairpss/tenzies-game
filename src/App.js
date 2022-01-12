4201505e19d06b5029247e0800ef5f285673fcb0
Jair Partida <jairpartida1@gmail.com>
2022-01-11 23:14:26 -0700

    map the array into die components

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Confetti from "react-confetti"
 import Die from "./Die"
 
 function App() {
-    
+    const [dice, setDice] = useState(allNewDice())
+
+
+    function allNewDice() {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random()*6))
+      }
+      return newDice
+    }
+
+    const diceElements = dice.map(die => <Die value={die} />)
+
     return (
         <main>
             <div className="die-container">
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
+               {diceElements}
             </div>
         </main>
     )
