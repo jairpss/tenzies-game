9f046ff24120e5b66119b130eb9fc95da432e876
Jair Partida <jairpartida1@gmail.com>
2022-05-15 21:20:13 -0600

    fixing typos

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
         <main>
             {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <p className="instructions">Roll until all dice are the same. Click each dice to freeze it at its current value between rolls.</p>
             <div className="die-container">
                {diceElements}
             </div>
